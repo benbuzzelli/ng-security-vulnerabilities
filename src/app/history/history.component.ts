e3660d572cb42ad14ff6e7b0b3e445ac6b2dd796
henry5798 <henryalvarez5798@gmail.com>
2020-09-02 17:53:35 -0400

    Added History Table with Dummy Data

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,6 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from  "@angular/router";
 
+export interface HistoryTable {
+  date: string;
+  github: string;
+  description: string;
+  vulnerable: string;
+  severity: string;
+}
+
+const HISTORY_DATA: HistoryTable[] = [
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 1.0079, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 4.0026, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 6.941, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 9.0122, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 10.811, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 12.0107, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 14.0067, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 15.9994, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 18.9984, vulnerable: 'Yes', severity: '100%' },
+  { date: '09/02/2020', github: 'https://github.com/benbuzzelli/ng-security-vulnerabilities', description: 20.1797, vulnerable: 'Yes', severity: '100%' },
+];
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
@@ -9,7 +30,8 @@ import { Router } from  "@angular/router";
 export class HistoryComponent implements OnInit {
 
   constructor(public router: Router) { }
-
+  displayedColumns: string[] = ['date', 'github', 'description', 'vulnerable', 'severity'];
+  dataSource = HISTORY_DATA;
   ngOnInit(): void {
   }
 
