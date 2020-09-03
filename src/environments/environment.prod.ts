4b7dce7b197361d68c333b1d5ae0adfb96d18ebf
henry5798 <henryalvarez5798@gmail.com>
2020-09-03 13:32:32 -0400

    Updated Budgets and fixed deploy issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCNREzzNW83QsUIgG6QzX-ozpOINWL955c",
+    authDomain: "security-whale.firebaseapp.com",
+    databaseURL: "https://security-whale.firebaseio.com",
+    projectId: "security-whale",
+    storageBucket: "security-whale.appspot.com",
+    messagingSenderId: "774091153639",
+    appId: "1:774091153639:web:69b48562282a96662e0242",
+    measurementId: "G-PZCYMMGZ1Q"
+  }
 };
